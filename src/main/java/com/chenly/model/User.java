4db4653e7f5763becd979b6fc459668e4bd7cee5
duiduiya <1653516743@qq.com>
2019-05-24 11:13:56 +0800

    注册页面

---
+++ src/main/java/com/chenly/model/User.java
@@ -0,0 +1,12 @@
+package com.chenly.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+@Data
+public class User implements Serializable {
+    private Integer id;
+    private String username;
+    private String password;
+    private String email;
+}
